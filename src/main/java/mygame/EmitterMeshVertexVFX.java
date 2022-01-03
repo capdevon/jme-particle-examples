28a5cea220bf929bf731d75ee0427fe9666e37f1
Wyatt Gillette <jefferydeaver2010@gmail.com>
2022-01-03 23:17:00 +0100

    Add files via upload

---
+++ src/main/java/mygame/EmitterMeshVertexVFX.java
@@ -0,0 +1,98 @@
+package mygame;
+
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.jme3.effect.shapes.EmitterShape;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.VertexBuffer;
+import com.jme3.util.BufferUtils;
+import com.jme3.util.clone.Cloner;
+
+/**
+ *
+ * @author capdevon
+ */
+public class EmitterMeshVertexVFX implements EmitterShape {
+
+    private Mesh source;
+    private List<Integer> indexList = new ArrayList<>();
+
+    public EmitterMeshVertexVFX(Mesh source) {
+        this.source = source;
+        calculateVertices();
+    }
+
+    private void calculateVertices() {
+        Map<Vector3f, Integer> mapPos = new HashMap<>(source.getVertexCount());
+
+        // Get the geometry's vertex buffer
+        FloatBuffer vertexBuffer = source.getFloatBuffer(VertexBuffer.Type.Position);
+        for (int i = 0; i < vertexBuffer.limit() / 3; i++) {
+
+            Vector3f position = new Vector3f();
+            BufferUtils.populateFromBuffer(position, vertexBuffer, i);
+
+            if (!mapPos.containsKey(position)) {
+                mapPos.put(position, i);
+            }
+        }
+
+        indexList.addAll(mapPos.values());
+        System.out.println("IndexList: " + indexList.size());
+    }
+
+    @Override
+    public void getRandomPoint(Vector3f store) {
+        int vertIndex = FastMath.nextRandomInt(0, indexList.size() - 1);
+        FloatBuffer vertexBuffer = source.getFloatBuffer(VertexBuffer.Type.Position);
+        BufferUtils.populateFromBuffer(store, vertexBuffer, vertIndex);
+    }
+
+    @Override
+    public void getRandomPointAndNormal(Vector3f store, Vector3f normal) {
+        int vertIndex = FastMath.nextRandomInt(0, indexList.size() - 1);
+
+        FloatBuffer vertBuffer = source.getFloatBuffer(VertexBuffer.Type.Position);
+        BufferUtils.populateFromBuffer(store, vertBuffer, vertIndex);
+
+        FloatBuffer normBuffer = source.getFloatBuffer(VertexBuffer.Type.Normal);
+        BufferUtils.populateFromBuffer(normal, normBuffer, vertIndex);
+    }
+
+    @Override
+    public EmitterShape deepClone() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public Object jmeClone() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void cloneFields(Cloner cloner, Object original) {
+        // TODO Auto-generated method stub
+    }
+
+}
---
+++ src/main/java/mygame/Test_JmeVFX.java
@@ -0,0 +1,232 @@
+package mygame;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.jme3.anim.AnimComposer;
+import com.jme3.anim.SkinningControl;
+import com.jme3.app.DetailedProfilerState;
+import com.jme3.app.SimpleApplication;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.Trigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.material.Materials;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.post.filters.BloomFilter;
+import com.jme3.post.filters.FXAAFilter;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.CenterQuad;
+import com.jme3.shadow.DirectionalLightShadowFilter;
+import com.jme3.system.AppSettings;
+import com.jme3.texture.Texture;
+
+/**
+ *
+ * @author capdevon
+ */
+public class Test_JmeVFX extends SimpleApplication implements ActionListener {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Test_JmeVFX app = new Test_JmeVFX();
+        AppSettings settings = new AppSettings(true);
+        settings.setResolution(1024, 768);
+        app.setSettings(settings);
+        app.setPauseOnLostFocus(false);
+        app.setShowSettings(false);
+        app.start();
+    }
+
+    private boolean autoRotate = false;
+    private Node myModel;
+    private AnimComposer animComposer;
+    private Queue<String> animsQueue = new LinkedList<>();
+    private BitmapText animUI;
+
+    @Override
+    public void simpleInitApp() {
+
+        BitmapText hud = createTextUI(ColorRGBA.White, 20, 15);
+        hud.setText("NextAnim: KEY_RIGHT, AutoRotate: KEY_RETURN");
+
+        animUI = createTextUI(ColorRGBA.Blue, 20, 15 * 2);
+        animUI.setText("Current Animation: NULL");
+
+        //Press F6 to turn it on/off.
+        stateManager.attach(new DetailedProfilerState());
+
+        configCamera();
+        setupLights();
+        setupScene();
+        setupCharacter();
+        setupKeys();
+    }
+
+    private void setupCharacter() {
+        // Add model to the scene
+        myModel = (Node) assetManager.loadModel("Models/Mixamo/ybot.j3o");
+        rootNode.attachChild(myModel);
+
+        animComposer = GameObject.findControl(myModel, AnimComposer.class);
+        animsQueue.addAll(animComposer.getAnimClipsNames());
+        System.out.println(animsQueue);
+
+        SkinningControl skControl = GameObject.findControl(myModel, SkinningControl.class);
+        skControl.setHardwareSkinningPreferred(false);
+
+        // 1. Alpha_Surface
+        // 2. Alpha_Joints
+        Geometry geo = (Geometry) myModel.getChild("Alpha_Surface");
+        //vertexMode(geo);
+        particleMode(geo);
+    }
+
+    private void vertexMode(Geometry source) {
+        Node node = new Node("SkinnedMesh");
+        SkinnedVFXControl skinVFX = new SkinnedVFXControl(source.getMesh(), 0.001f);
+        skinVFX.mat.setColor("GlowColor", ColorRGBA.Magenta);
+        skinVFX.target = source.getParent();
+        skinVFX.refreshRate = 0.01f;
+        node.addControl(skinVFX);
+        rootNode.attachChild(node);
+    }
+
+    private void particleMode(Geometry geo) {
+        ParticleEmitter emit = new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 300);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Particle.j3md");
+        mat.setTexture("Texture", assetManager.loadTexture("Effects/Smoke/Smoke.png"));
+        //mat.setColor("GlowColor", ColorRGBA.Magenta);
+        emit.setMaterial(mat);
+        emit.setLowLife(1);
+        emit.setHighLife(1);
+        emit.setImagesX(15);
+        emit.setStartSize(0.02f);
+        emit.setEndSize(0.02f);
+        emit.setStartColor(ColorRGBA.Blue);
+        emit.setEndColor(ColorRGBA.Cyan);
+        emit.setParticlesPerSec(100);
+        emit.setGravity(0, 0, 0);
+        emit.getParticleInfluencer().setVelocityVariation(1);
+        emit.getParticleInfluencer().setInitialVelocity(new Vector3f(0, .5f, 0));
+        emit.setShape(new EmitterMeshVertexVFX(geo.getMesh()));
+        //emit.setShape(new EmitterMeshFaceVFX(geo.getMesh()));
+        emit.setLocalTransform(geo.getParent().getWorldTransform());
+        myModel.attachChild(emit);
+    }
+
+    private void setupKeys() {
+        addMapping("autoRotate", new KeyTrigger(KeyInput.KEY_RETURN));
+        addMapping("nextAnim", new KeyTrigger(KeyInput.KEY_RIGHT));
+    }
+
+    private void addMapping(String mappingName, Trigger... triggers) {
+        inputManager.addMapping(mappingName, triggers);
+        inputManager.addListener(this, mappingName);
+    }
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals("nextAnim") && isPressed) {
+            String anim = animsQueue.poll();
+            animsQueue.add(anim);
+            animComposer.setCurrentAction(anim);
+            animUI.setText("Current Animation: " + anim);
+
+        } else if (name.equals("autoRotate") && isPressed) {
+            autoRotate = !autoRotate;
+        }
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        if (autoRotate) {
+            myModel.rotate(0, tpf, 0);
+        }
+    }
+
+    private void configCamera() {
+        flyCam.setDragToRotate(true);
+        flyCam.setMoveSpeed(10);
+
+        cam.setLocation(new Vector3f(0, 2, 5));
+        cam.lookAt(Vector3f.UNIT_Y, Vector3f.UNIT_Y);
+
+        float aspect = (float) cam.getWidth() / cam.getHeight();
+        cam.setFrustumPerspective(45, aspect, 0.1f, 1000f);
+    }
+
+    private void setupScene() {
+        CenterQuad quad = new CenterQuad(8, 8);
+        quad.scaleTextureCoordinates(new Vector2f(2, 2));
+        Geometry floor = createMesh("Floor", quad);
+        floor.rotate(-FastMath.HALF_PI, 0, 0);
+        rootNode.attachChild(floor);
+    }
+
+    private Geometry createMesh(String name, Mesh mesh) {
+        Geometry geo = new Geometry(name, mesh);
+        Material mat = new Material(assetManager, Materials.LIGHTING);
+        Texture tex = assetManager.loadTexture("Interface/Logo/Monkey.jpg");
+        tex.setWrap(Texture.WrapMode.Repeat);
+        mat.setTexture("DiffuseMap", tex);
+        geo.setMaterial(mat);
+        return geo;
+    }
+
+    private void setupLights() {
+        viewPort.setBackgroundColor(ColorRGBA.DarkGray);
+        rootNode.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(ColorRGBA.White);
+        //rootNode.addLight(ambient);
+
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun);
+
+        DirectionalLightShadowFilter dlsf = new DirectionalLightShadowFilter(assetManager, 4096, 3);
+        dlsf.setLight(sun);
+        dlsf.setShadowIntensity(0.4f);
+        dlsf.setShadowZExtend(256);
+
+        FXAAFilter fxaa = new FXAAFilter();
+        BloomFilter bloom = new BloomFilter(BloomFilter.GlowMode.Objects);
+
+        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+        fpp.addFilter(bloom);
+        fpp.addFilter(dlsf);
+        fpp.addFilter(fxaa);
+        viewPort.addProcessor(fpp);
+    }
+
+    private BitmapText createTextUI(ColorRGBA color, float xPos, float yPos) {
+        BitmapFont font = assetManager.loadFont("Interface/Fonts/Console.fnt");
+        BitmapText bmp = new BitmapText(font);
+        bmp.setSize(font.getCharSet().getRenderedSize());
+        bmp.setLocalTranslation(xPos, settings.getHeight() - yPos, 0);
+        bmp.setColor(color);
+        guiNode.attachChild(bmp);
+        return bmp;
+    }
+
+}
