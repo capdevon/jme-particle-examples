ffdb4649c6c7f885f5e1ef96c6d77351148dbd32
Wyatt Gillette <jefferydeaver2010@gmail.com>
2022-02-04 08:54:50 +0100

    Update MyParticleEmitter.java

--- src/main/java/mygame/effect/MyParticleEmitter.java
+++ src/main/java/mygame/effect/MyParticleEmitter.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2009-2021 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package mygame.effect;
 
 import java.io.IOException;
@@ -29,11 +60,11 @@ import com.jme3.util.TempVars;
 import com.jme3.util.clone.Cloner;
 
 /**
- * <code>ParticleEmitter</code> is a special kind of geometry which simulates a
- * particle system.
+ * <code>ParticleEmitter</code> is a special kind of geometry which simulates
+ * a particle system.
  * <p>
- * Particle emitters can be used to simulate various kinds of phenomena, such as
- * fire, smoke, explosions and much more.
+ * Particle emitters can be used to simulate various kinds of phenomena,
+ * such as fire, smoke, explosions and much more.
  * <p>
  * Particle emitters have many properties which are used to control the
  * simulation. The interpretation of these properties depends on the
@@ -41,7 +72,7 @@ import com.jme3.util.clone.Cloner;
  * {@link ParticleEmitter#setParticleInfluencer(com.jme3.effect.influencers.ParticleInfluencer) }.
  * By default, the {@link DefaultParticleInfluencer} implementation is used.
  *
- * @author capdevon
+ * @author Kirill Vainer
  */
 public class MyParticleEmitter extends Geometry {
 
